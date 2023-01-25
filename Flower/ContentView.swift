a87827e6bdea7bae872edfad6e91d003cb320160
ciliaence <cilia.ence@telia.com>
2023-01-25 17:28:55 +0100

    set up navigationlink for the list

--- Flower/ContentView.swift
+++ Flower/ContentView.swift
@@ -13,34 +13,51 @@ import FirebaseFirestore
 struct ContentView: View {
     
     @StateObject var stores = Stores()
+    @State var showingStoreSheet = false
+    @State var storeNumber = 0
+    
     var body: some View {
         TabView{
             List ($stores.list) { $store in
-                RowView(store: store)
-            }
-            
-            .tabItem {
-                Image(systemName: "house.fill")
-                Text("Home")
-            }
-            .onAppear(){
-                // saveToFirestore("Blombutiken", 99, "8 timmar", "flower4")
-                listenToFirestore()
-            }
-            Text("search view")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
+                
+                NavigationLink(destination: StoreView(store: store)){
+                    RowView(store: store)
                 }
-            Text("account view")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Account")
-                }
-            
+                
+                
+            }
+                
+                
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                    
+                Text("search view")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                Text("account view")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Account")
+                    }
+                
+                
+            }
+            //        .sheet(isPresented: $showingStoreSheet) {
+            //            StoreSheet()
+            //        }
+        
+        .onAppear(){
+            // saveToFirestore("Blombutiken", 99, "8 timmar", "flower4")
+            listenToFirestore()
         }
     }
-    
+                               
+                               
+                               
     func saveToFirestore (_ storeName : String, _ fee: Int, _ time : String, _ img: String) {
         let db = Firestore.firestore()
         let store = Store(name: storeName, deliveryFee: fee, deliveryTime: time, image: img)
@@ -85,6 +102,16 @@ struct ContentView: View {
     
 }
 
+struct StoreView : View {
+    
+    let store : Store
+    
+    var body: some View{
+        Text(store.name)
+    }
+    
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -102,12 +129,13 @@ struct RowView: View {
                 .resizable()
                 .frame(width: 400, height: 160)
             HStack{
+               
                 
                 Text(store.name)
-                HStack{
-                    Text("\(store.deliveryFee)kr")
-                    Text(store.deliveryTime)
-                }
+               
+                    Text("\(store.deliveryFee)kr - " + store.deliveryTime)
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
             }
         }
     }
