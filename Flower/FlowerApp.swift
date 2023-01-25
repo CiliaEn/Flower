c72adc1fe42c68cccf3537ad99717ed32c185c83
ciliaence <cilia.ence@telia.com>
2023-01-25 10:26:22 +0100

    first commit

---
+++ Flower/FlowerApp.swift
@@ -0,0 +1,34 @@
+//
+//  FlowerApp.swift
+//  Flower
+//
+//  Created by Cilia Ence on 2023-01-25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct FlowerApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
